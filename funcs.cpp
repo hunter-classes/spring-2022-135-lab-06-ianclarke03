93dc11752d8ebe26fd3fcf244c5535d06107ac9e
ianclarke03 <83726782+ianclarke03@users.noreply.github.com>
2022-03-10 20:27:33 +0000

    A

--- funcs.cpp
+++ funcs.cpp
@@ -3,3 +3,17 @@
 
 // add functions here
 
+int test_ascii (std::string s)
+  {
+    for (int i = 0; i < s.length(); i++)
+      {
+        std::cout << s[i] << ' ' << int(s[i]) << "\n";
+      }
+  }
+
+int main()
+{
+  std::cout << test_ascii ("Cat :3 Dog");
+
+  return 0;
+}
